b133859745c555d66b5a62d4e9778173502411da
chen ke <joyyouxiang@163.com>
2017-04-24 07:03:48 +0800

    20170424-home

---
+++ js_basic10/js/1.js
@@ -0,0 +1,21 @@
+/**
+ * Created by joy on 2017/4/24.--home
+ */
+window.onload = function () {
+    var aData = [
+        {
+            "num":1,
+            "name":"雪碧",
+            "price":2.5,
+            "count":35
+        },
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {}];
+}
